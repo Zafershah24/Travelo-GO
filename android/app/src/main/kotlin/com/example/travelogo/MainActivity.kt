53199db6ca55df926c0cc6a69e6caaad79e1eec7
zafer247 <zafershah247@gmail.com>
2020-07-25 01:06:30 +0530

    1st Commit

---
+++ android/app/src/main/kotlin/com/example/travelogo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.travelogo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,301 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = new TabController(length: 4, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+      bottomNavigationBar: Material(
+        color: Colors.white38,
+        child: TabBar(
+          controller: tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.home, color: Colors.black)),
+             Tab(icon: Icon(Icons.search, color: Colors.grey)),
+            Tab(icon: Icon(Icons.add_circle_outline, color: Colors.grey)),
+             Tab(icon: Icon(Icons.graphic_eq, color: Colors.grey)),
+
+          ],
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 15.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'TRAVELO-GO',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey.shade900),
+                ),
+                SizedBox(width: 105.0),
+                IconButton(
+                  icon: Icon(Icons.notifications),
+                  color: Colors.white,
+                  iconSize: 30.0,
+                  onPressed: () {},
+                ),
+                SizedBox(width: 8.0),
+                InkWell(
+                  child: Container(
+                    height: 50.0,
+                    width: 50.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        image: DecorationImage(
+                            image: AssetImage('assets/zafer.jpg'))),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 15.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 10.0),
+              height: 80.0,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.grey[300]),
+              child: Row(
+                children: <Widget>[
+              Transform.rotate( angle: 45* math.pi/180 ,
+                child: IconButton(
+                  icon: Icon(Icons.navigation, color: Colors.pinkAccent),
+                  iconSize: 39.0,
+                  onPressed: () {},
+                ),
+              ),
+
+                  SizedBox(width: 85.0),
+                  Padding(
+                    padding: EdgeInsets.only(top: 27.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'MALDIVES',
+                          style: TextStyle(
+                              color: Colors.grey.shade500, fontSize: 16.0),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          'Add an update',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 30.0),
+                  IconButton(
+                    icon: Icon(Icons.arrow_forward_ios, color: Colors.grey),
+                    iconSize: 30.0,
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'FROM THE TEAM',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontFamily: 'Montserrat'),
+                ),
+                Text(
+                  'View All',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                      fontFamily: 'Montserrat'),
+                )
+              ],
+            ),
+          ),
+          _buildImageGrid(),
+          _imgGalleryDetail(),
+          _buildImageGrid(),
+          _imgGalleryDetail()
+        ],
+      ),
+    );
+  }
+
+  Widget _imgGalleryDetail() {
+    return Padding(
+      padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 15.0),
+      child: Row(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Maldives Summer',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Montserrat',
+                    fontSize: 15.0),
+              ),
+              SizedBox(height: 7.0),
+              Row(
+                children: <Widget>[
+                  Text(
+                    'Zafer added 52 Photos',
+                    style: TextStyle(
+                        color: Colors.grey.shade700,
+                        fontFamily: 'Montserrat',
+                        fontSize: 11.0),
+                  ),
+                  SizedBox(width: 4.0),
+                  Icon(
+                    Icons.timer,
+                    size: 4.0,
+                    color: Colors.black,
+                  ),
+                  SizedBox(width: 4.0),
+                  Text(
+                    '1h ago',
+                    style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontFamily: 'Montserrat',
+                        fontSize: 11.0),
+                  )
+                ],
+              )
+            ],
+          ),
+          SizedBox(width: 79.0),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(width: 7.0),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 16.0,
+                  width: 14.0,
+                  child: Image.asset('assets/navarrow.png'),
+                ),
+              ),
+              SizedBox(width: 7.0),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 20.0,
+                  width: 20.0,
+                  child: Image.asset('assets/chatbubble.png'),
+                ),
+              ),
+              SizedBox(width: 7.0),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 22.0,
+                  width: 22.0,
+                  child: Image.asset('assets/fav.png'),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImageGrid() {
+    return Padding(
+        padding: EdgeInsets.only(top: 25.0, left: 15.0, right: 15.0),
+        child: Container(
+          height: 225.0,
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 225.0,
+                width: MediaQuery.of(context).size.width / 2 + 40.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15.0),
+                        bottomLeft: Radius.circular(15.0)),
+                    image: DecorationImage(
+                        image: AssetImage('assets/beach3.jpg'),
+                        fit: BoxFit.cover)),
+              ),
+              SizedBox(width: 2.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 111.5,
+                    width: MediaQuery.of(context).size.width / 2 - 72.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15.0),
+                        ),
+                        image: DecorationImage(
+                            image: AssetImage('assets/beach2.jpg'),
+                            fit: BoxFit.cover)),
+                  ),
+                  SizedBox(height: 2.0),
+                  Container(
+                    height: 111.5,
+                    width: MediaQuery.of(context).size.width / 2 - 72.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(15.0)),
+                        image: DecorationImage(
+                            image: AssetImage('assets/beach1.jpg'),
+                            fit: BoxFit.cover)),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
